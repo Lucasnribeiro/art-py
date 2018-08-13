aba71af85fbe91076e7477355a35d6e9a59cb924
Lucas <29964166+xTriumph@users.noreply.github.com>
2018-08-13 14:16:18 -0300

    Update tumblr-API.py
    I uploaded the wrong code. Fixed now

--- tumblr-API.py
+++ tumblr-API.py
@@ -2,21 +2,31 @@ import pytumblr
 import json
 import time
 import os
+import re
 
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    
+    return [ atoi(c) for c in re.split('(\d+)', text) ]
 
 # Authenticate via OAuth
 client = pytumblr.TumblrRestClient(
-
-  '                                                  ',
-  '                                                  ',
-  '                                                  ',
-  '                                                  '
+  'gPsUK6nFPuNBFJuDCCsQzVwLJ9Z7AlJr3qdDB8mkUsIWgfoyA5',
+  'iCVP39SdfDsZqQ1xQm5unl32N15eO8m8Mwx3niZRKu0gRiMR1T',
+  'jKP12UV9C1Xta6oonzwYkbpuNSDJUQE64L6hDSLVJPk46nepas',
+  'ZIFJq6xhuyuVjmYaEvfOmIByL3QA6MIewJlkJjj7sfZzBQ8Evz'
 )
 
 # Make the request
 blog = client.blog_info('artstation-py')
 
 os.chdir('artstation')
+current_dir = os.getcwd()
+print(current_dir)
+db_avoid = ["link_db.txt", "name_db.txt", "title_db.txt"]
+
 
 
 with open('name_db.txt') as name:
@@ -28,19 +38,27 @@ with open('link_db.txt') as link:
 
 with open('title_db.txt') as title:
 	title_bot = json.load(title)
-      
+
+dir_images = os.listdir('.')
+dir_images.sort(key=natural_keys)
+print(dir_images)
+
+current_dir2 = os.getcwd()
+print(current_dir2)
+'''print(ordered_files)'''
+   
 counter = 0
-for artwork_image in os.listdir('.'):
-    print(artwork_image)
-    print(title_bot)
-    post = "Artist: "
-    post += names_bot[counter]
-    post += "\nSource: " 
-    post += source_bot[counter]
-    if artwork_image == title_bot[counter]:
-    	client.create_photo('artstation-py', state="published", tags=["artstation"],
-    						caption=post,
-    						data=artwork_image)
-    else:
-    	print("Failed to find image")	
+
+for artwork_image in dir_images:
+    time.sleep(40)
+    if artwork_image not in db_avoid: 
+    		print(artwork_image)
+
+	    	post = "Artist: "
+    		post += names_bot[counter]
+    		post += "\nSource: " 
+    		post += source_bot[counter]
+    		client.create_photo('artstation-py', state="published", tags=["artstation"],
+    							caption=post,
+    							data=artwork_image)	
     counter += 1
