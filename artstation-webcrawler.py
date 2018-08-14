eddadf4ddab4b8b43a7418eb30e9fa84bd4dcba6
Lucas <29964166+xTriumph@users.noreply.github.com>
2018-08-14 00:08:04 -0300

    Infinite loop through pages + fix indentation
    Now it has the functionality of going through all the pages infinitely! Really useful if you want to gather A LOT of images. Fixed a minor indentation issue.

--- artstation-webcrawler.py
+++ artstation-webcrawler.py
@@ -12,47 +12,56 @@ info_list = list()
 img_link_bot = ""
 artwork_name_bot = ""
 title_db = list()
-pages = ['1', '2', '3', '4', '5', '6']
 
 if not os.path.exists('artstation'):
-    os.makedirs('artstation', mode=0o777)
+	os.makedirs('artstation', mode=0o777)
 # move to new directory
 os.chdir('artstation')
 
 
-pass
+loop_count = 1
 counter = 0
-url = 'https://www.artstation.com/projects.json?page=1&sorting=trending'
-page = requests.get(url)
-json_data = json.loads(page.text)
+original_url = 'https://www.artstation.com/projects.json?page={}&sorting=trending'
 
 
-for data in json_data['data']:
-	print("Image: ", counter)
-	time.sleep(1)
-	permalink.append(data['permalink'])
-	img_link.append(data['cover']['medium_image_url'])
-	full_name.append(data['user']['full_name'])
-	artwork_name.append(data['title'])
-	with open('link_db.txt', 'w') as filehandle:	
-		json.dump(permalink , filehandle)
-	
-	with open('name_db.txt', 'w') as filehandle:	
-		json.dump(full_name , filehandle)
 
-	artwork_name_bot = data['title']
-	clean_name = re.sub('[^A-Za-z0-9]+','', artwork_name_bot)
-	title_db.append(clean_name)
+while True:
+	url = original_url.format(loop_count)
+	print(url)
+	print(original_url)
+	page = requests.get(url)
+	json_data = json.loads(page.text)
+
+
+	for data in json_data['data']:
+		print("Image: ", counter)
+		time.sleep(1)
+		permalink.append(data['permalink'])
+		img_link.append(data['cover']['medium_image_url'])
+		full_name.append(data['user']['full_name'])
+		artwork_name.append(data['title'])
+		with open('link_db.txt', 'w') as filehandle:	
+			json.dump(permalink , filehandle)
 	
-	with open('title_db.txt', 'w') as filehandle:
-		json.dump(title_db , filehandle)
-	
-	img_link_bot = data['cover']['medium_image_url']
+		with open('name_db.txt', 'w') as filehandle:	
+			json.dump(full_name , filehandle)
+
+		artwork_name_bot = data['title']
+		clean_name = re.sub('[^A-Za-z0-9]+','', artwork_name_bot)
+		title_db.append(clean_name)
 		
-	print(clean_name)
-	img_link_bot = data['cover']['medium_image_url']
-	img_file = requests.get(img_link_bot).content 
-	with open("{}".format(counter) + "-" + clean_name + '.jpg', 'wb') as handler:
-    		handler.write(img_file)
-    		counter += 1
-print(artwork_name)
+		with open('title_db.txt', 'w') as filehandle:
+			json.dump(title_db , filehandle)
+			
+		img_link_bot = data['cover']['medium_image_url']
+		
+		print(clean_name)
+		img_link_bot = data['cover']['medium_image_url']
+		img_file = requests.get(img_link_bot).content 
+		with open("{}".format(counter) + "-" + clean_name + '.jpg', 'wb') as handler:
+			handler.write(img_file)
+		counter += 1
+	
+	loop_count += 1
+	print('Im out of the loop')
+	print(loop_count)			
